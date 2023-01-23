bb9c5e01cfdc92c0ff78736683a9a0bac7eb8cf1
Rafael <rflpeq.rs@gmail.com>
2023-01-23 19:54:53 -0300

    Parou em pesquisa lowercase xadrez

---
+++ fundamentos/secao-03-introdução-a-javascript/dia-01-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,61 @@
+// const a = 10;
+// const b = 20;
+
+// const adição = a + b;
+// const subtração = a - b;
+// const multiplicação = a * b;
+// const divisão = a / b;
+// const modulo = a % b;
+
+// console.log(adição);
+// console.log(subtração);
+// console.log(multiplicação);
+// console.log(divisão);
+// console.log(modulo);
+
+// a = 10;
+// b = 20;
+// let maiorNumero;
+// if (a > b) {
+//     maiorNumero = a;
+// } else {
+//     maiorNumero = b;
+// }
+// console.log(maiorNumero);
+
+// let maiorDeTres;
+// a = 10;
+// b = 20;
+// const c = 30;
+// if (a > b & c) {
+//     maiorDeTres = a;
+// } else if (b > a & c) {
+//     maiorDeTres = b;
+// } else {
+//     maiorDeTres = c;
+// }
+// console.log(maiorDeTres);
+
+// let retorno = '';
+// let parametro = -1;
+// if (parametro === 0) {
+//     retorno = 'zero';
+// } else if (parametro > 0) {
+//     retorno = 'positivo';
+// } else {
+//     retorno = 'negativo';
+// }
+// console.log(retorno);
+
+
+const anguloA = 70;
+const anguloB = 60;
+const anguloC = 60;
+let triangulo;
+
+if (anguloA + anguloB + anguloC === 180) {
+    triangulo = true;
+} else {
+    triangulo = false + ' Não é triangulo';
+}
+console.log(triangulo);
