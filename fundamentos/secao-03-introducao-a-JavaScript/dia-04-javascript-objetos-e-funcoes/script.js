f71d020b81ca546506704fdcfcaba2fed719a003
Rafael <rflpeq.rs@gmail.com>
2023-01-26 20:03:14 -0300

    resolução dos exercicios, parou em imprimir a frase com livro favorito

---
+++ fundamentos/secao-03-introducao-a-JavaScript/dia-04-javascript-objetos-e-funcoes/script.js
@@ -0,0 +1,47 @@
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+// console.log('Bem-vinda,' , info.personagem);
+
+info.recorrente = 'Sim';
+// console.log(info.recorrente);
+
+// for (let index in info) {
+//     console.log(index);
+// }
+
+// for (let index in info) {
+//     console.log(info[index]);
+// }
+
+// let info2 = {
+//     personagem: 'Tio Patinhas',
+//     origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
+//     nota: 'O último MacPatinhas',
+//     recorrente: 'Sim'
+// }
+
+// for (let index in info) {
+//     if (index === 'recorrente' && info[index] === 'Sim' && info2[index] === 'Sim') {
+//         console.log('Amboms são recorrentes')
+//     } else {
+//         console.log(info[index], ' e ', info2[index]);
+//     }
+// } 
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+  console.log('O livro favorito de ', leitor.nome, leitor.sobrenome, 'se chama ', leitor['livrosFavoritos']['titulo']);
+
