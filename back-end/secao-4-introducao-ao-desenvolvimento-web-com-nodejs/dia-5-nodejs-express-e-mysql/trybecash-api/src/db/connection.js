9c7e2095167d210384411ec885d202ce2346eeda
Rafael <rflpeq.rs@gmail.com>
2023-07-20 06:08:47 -0300

    Conteúdo do course funcionando até as variáveis de ambiente

---
+++ back-end/secao-4-introducao-ao-desenvolvimento-web-com-nodejs/dia-5-nodejs-express-e-mysql/trybecash-api/src/db/connection.js
@@ -0,0 +1,16 @@
+// src/db/connection.js
+
+const mysql = require('mysql2/promise');
+
+const connection = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+module.exports = connection;
